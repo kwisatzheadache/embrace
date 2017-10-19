80233113cb88a0b2bf1d21804e7b927f54570a7c
kwisatzheadache <michaelhorvath87@gmail.com>
2017-10-19 13:19:51 -0700

    lagging entire combined sets

--- start.py
+++ start.py
@@ -1,5 +1,10 @@
 import matplotlib.pyplot as plt
 import numpy as np
+from statsmodels.tsa.ar_model import AR
+from sklearn.metrics import mean_squared_error
+from math import sqrt
+from pandas import DataFrame
+from pandas import concat
 
 stand_sit = np.genfromtxt('stand_sit.csv', delimiter=",")
 walk1 = np.genfromtxt('walking.csv', delimiter=",")
@@ -36,9 +41,37 @@ combined = np.vstack((walk_combined, stand_sit_combined))
 
 """ Begin modeling for walking """
 
-from statsmodels.tsa.ar_model import AR
-from sklearn.metrics import mean_squared_error
-from math import sqrt
-
 # Determine what is the best window for the classification models
+# I should have the binary class models picked out already. Now,
+# iteratively run through different window sizes, testing the model fit.
 
+
+# Creates a df with columns [T, t-1, t-2, ... t-window]
+def lag(variable, window):
+    df1 = DataFrame(variable)
+    for i in range(window):
+        j = window - i
+        df1 = concat([df1, variable.shift(j)], axis=1)
+    columns = [variable.name]
+    for i in range(window):
+        j = window - i
+        columns.append(variable.name + ' t - %d' % j)
+    df1.columns = columns
+    return df1.iloc[window:]
+
+df_walk = DataFrame(walk_combined)
+df_stand_sit = DataFrame(stand_sit_combined)
+
+cols = ['id', 'acc_x', 'acc_y', 'acc_z', 'gy_x', 'gy_y', 'gy_z', 'mag_x', 'mag_y', 'mag_z', 'y']
+df_walk.columns = cols
+df_stand_sit.columns = cols
+
+def lag_set(df, window):
+    for column in df:
+        lagged = lag(df[column], window)
+        cols = lagged.columns
+        print df[cols]
+        # df[cols] = lagged
+
+to_lag = cols[1:-1]
+window = 10
