f3bc1fd3ecae57fe4321748e988f44e19683e6ed
kwisatzheadache <michaelhorvath87@gmail.com>
2017-10-18 20:58:57 -0700

    start lag function

--- start.py
+++ start.py
@@ -17,18 +17,28 @@ Perhaps the easiest place to start is just to label walk1 and walk2 as 1, and st
 train a binary classifier to recognize walking. I think I'll do that.
 """
 
-walk_combined = np.vstack([walk1, walk2])
+# Create the label variable and append it to the data
+walk_both = np.vstack([walk1, walk2])
 walk_y = []
-for i in walk_combined:
+for i in walk_both:
     walk_y.append([1])
 walk_y = np.array(walk_y)
+walk_combined = np.hstack((walk_both, walk_y))
 
-walk_combined = np.hstack((walk_combined, walk_y))
-
+# Again for the stand_sit data
 stand_sit_y = []
 for i in stand_sit:
     stand_sit_y.append([0])
 stand_sit_y = np.array(stand_sit_y)
-stand_sit = np.hstack((stand_sit, stand_sit_y))
+stand_sit_combined = np.hstack((stand_sit, stand_sit_y))
 
-combined = np.vstack((walk_combined, stand_sit))
+combined = np.vstack((walk_combined, stand_sit_combined))
+
+""" Begin modeling for walking """
+
+from statsmodels.tsa.ar_model import AR
+from sklearn.metrics import mean_squared_error
+from math import sqrt
+
+# Determine what is the best window for the classification models
+
