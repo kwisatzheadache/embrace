60305a46c18fa336d0099bd04c1f071f2b72a544
kwisatzheadache <michaelhorvath87@gmail.com>
2017-10-22 17:38:45 -0700

    fix indexing error

--- random_sequence.py
+++ random_sequence.py
@@ -1,14 +1,33 @@
 import numpy as np
 from pandas import DataFrame
 from numpy.fft import fftn
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LogisticRegression
+from sklearn.cluster import DBSCAN
 
+execfile('fft.py')
 execfile('start.py')
-execfile('stand_sit.py')
-execfile('walk_binary.py')
+# execfile('stand_sit.py')
+# execfile('walk_binary.py')
 
-ran = DataFrame(np.genfromtxt('random_seq.csv', delimiter=",")).dropna()
-rand = ran.drop([ran.columns[0]], axis=1)
-random = DataFrame(fftn(rand))
+ran = np.genfromtxt('random_seq.csv', delimiter=",")
+
+random_fft = data_transform(ran, 15)
+walk_fft = data_transform(np.vstack((walk1, walk2)), 15)
+stand_sit_fft = data_transform(stand_sit, 15)
+
+y_walk = list()
+for i in range(len(walk_fft)):
+    y_walk.append(1)
+for i in range(len(stand_sit_fft)):
+    y_walk.append(0)
+
+X = np.vstack((walk_fft, stand_sit_fft))
+Y = y_walk
+X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=.33, random_state=8)
+walk_model = LogisticRegression()
+walk_model.fit(X_train, Y_train)
+# stand_sit_model = DBScan(eps=0.5, min_samples=10).fit(stand_sit_fft)
 
 """
 stand_sit is used to train stand_sit_model
@@ -21,17 +40,17 @@ output: 2=walk, 3=not_walk
 """
 
 walk_predict=walk_model.predict(random)
-stand_sit_predict = stand_sit_model.fit_predict(random)
+# stand_sit_predict = stand_sit_model.fit_predict(random)
 
 random_predict = []
 
-for i in walk_predict:
-    random_predict.append(i)
+# for i in walk_predict:
+#     random_predict.append(i)
 
-for i in range(len(random_predict)):
-    if random_predict[i] == 3:
-        random_predict[i] = stand_sit_predict[i]
-    else:
-        continue
+# for i in range(len(random_predict)):
+#     if random_predict[i] == 3:
+#         random_predict[i] = stand_sit_predict[i]
+#     else:
+#         continue
 
-print(random_predict)
+# print(random_predict)
--- start.py
+++ start.py
@@ -2,32 +2,13 @@ import matplotlib.pyplot as plt
 import numpy as np
 from pandas import DataFrame
 from pandas import concat
+import sys 
 
-
-# Function receives the df, xyz columns, and a window size; returns a new df with xyz lagged
-# according to window size, with id and y untouched.
-def lag_set(df, lag_variables, window):
-    df1 = DataFrame(df[['id', 'y']])
-    for column in lag_variables:
-        lagged = lag(df[column], window)
-        cols = lagged.columns
-        df1[cols] = lagged
-    return df1[:][window:]
-
-
-stand_sit = np.genfromtxt('stand_sit.csv', delimiter=",")
-walk1 = np.genfromtxt('walking.csv', delimiter=",")
-walk2 = np.genfromtxt('walking_2.csv', delimiter=",")
-
-indices = {'id':0, 'acc_x':1, 'acc_y':2, 'acc_z':3,
-           'gy_x':4, 'gy_y':5, 'gy_z':6,
-           'mag_x':7, 'mag_y':8, 'mag_z':9}
-
-"""
-Input: dataset, window
-Output: list of [Fk, n_freq]
-"""
 def lag(variable, window):
+    """
+    Input: dataset, window
+    Output: list of [Fk, n_freq]
+    """
     df = DataFrame(variable)
     for i in range(window):
         j = window - i
@@ -42,17 +23,44 @@ def dataset_to_windows(dataset, windowsize):
            windows.append(lag(dataset[:,i], windowsize))
     return np.array(windows)
 
-# INPUT dataset_to_windows output
-# OUTPUT 3 dimensional array
 def fft_transform(windows):
+    # INPUT dataset_to_windows output
+    # OUTPUT 3 dimensional array
     arr_windows = list()
     for i in range(len(windows)):
         arr_transforms = list()
         for j in range(len(windows[i])):
-            arr_transforms.append(get_fft(windows[i,j,:]))
+            try:
+                arr_transforms.append(get_fft(windows[i][j]))
+            except IndexError:
         arr_windows.append(arr_transforms)
+        try:
+            tmp = np.array(arr_windows)
+        except IndexError:
+            print(arr_windows.shape())
     return np.array(arr_windows)
 
-windows = dataset_to_windows(walk1, 15)
+def data_transform(dataset, windowsize):
+    windows = dataset_to_windows(dataset, windowsize)
+    return fft_transform(windows)
 
-fft_transformed = fft_transform(windows)
+def get_fft(signal):
+    # INPUT: [N] array of values
+    # OUTPUT: [Fk, frequ], list of coeff, list of freq values
+    print signal.shape
+    fs = 100
+    Fk = np.fft.rfft(signal)/float(len(signal))
+    f = np.fft.rfftfreq(len(signal), 1./fs)
+    return Fk, f
+
+def get_dom_freq(Fk, n_freq):
+    # INPUT: [Fk] (list of coeff)
+    #        n_freq (number of freq)
+    # OUTPUT: [Ind] (list of indices of dominant frequencies)
+    Fk_mag = np.abs(Fk)**2
+    Fk_mag_sort = np.abs(Fk)**2
+    Fk_mag_sort.sort()
+    idx = np.zeros(n_freq)
+    for i in range(n_freq):
+        idx[i] = np.where(Fk_mag == Fk_mag_sort[-(1+i)])[0]
+    return idx.astype(int)
