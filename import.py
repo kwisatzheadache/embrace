83a13b274259accc934b4525d19caab9adcd087a
kwisatzheadache <michaelhorvath87@gmail.com>
2017-10-24 16:27:00 -0700

    begin analyses with fft

--- import.py
+++ import.py
@@ -1,14 +1,15 @@
+from sklearn.cluster import DBSCAN
 
-stand_sit_with = np.genfromtxt('stand_sit.csv', delimiter=",")
-remove_nan = DataFrame(stand_sit_with).dropna()
-stand_sit = np.array(remove_nan)
-walk1 = np.genfromtxt('walking.csv', delimiter=",")
-walk2 = np.genfromtxt('walking_2.csv', delimiter=",")
+stand_sit_with = np.genfromtxt('data/stand_sit.csv', delimiter=",")
+walk1 = np.genfromtxt('data/walking.csv', delimiter=",")
+walk2 = np.genfromtxt('data/walking_2.csv', delimiter=",")
+stand = np.genfromtxt('data/stands.csv', delimiter=",")
+remove_nan = DataFrame(stand).dropna()
+stands= np.array(remove_nan)
+sit = np.genfromtxt('data/sits.csv', delimiter=",")
+remove_nan = DataFrame(sit).dropna()
+sits = np.array(remove_nan)
+random = np.genfromtxt('data/random_seq.csv', delimiter=",")
 
-# indices = {'id':0, 'acc_x':1, 'acc_y':2, 'acc_z':3,
-#            'gy_x':4, 'gy_y':5, 'gy_z':6,
-#            'mag_x':7, 'mag_y':8, 'mag_z':9}
+random_transformed = data_transform(random, 15)
 
-
-# windows = dataset_to_windows(walk1, 15)
-# fft_transformed = fft_transform(windows)
