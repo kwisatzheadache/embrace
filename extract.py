752cf5fd717bd1882af712f44c21b433ee1d41cf
kwisatzheadache <michaelhorvath87@gmail.com>
2017-10-25 20:31:36 -0700

    returns dominant frequencies

--- extract.py
+++ extract.py
@@ -1,5 +1,6 @@
+cols = ['acc_x', 'acc_y', 'acc_z', 'gy_x', 'gy_y', 'gy_z', 'mag_x', 'mag_y', 'mag_z'] 
 
-new = []
+df = DataFrame()
 for i in range(len(random_transformed)):
     col = []
     for j in range(len(random_transformed[1,:])):
@@ -7,8 +8,9 @@ for i in range(len(random_transformed)):
             print(i)
         Fk, n_freq = random_transformed[i,j]
         dom = get_dom_freq(Fk, 8)
-        col.append(dom)
-    np.hstack([new, col])
+        freqs = Fk[[dom]]
+        col.append(freqs)
+    df[cols[i]] = col
 
 # array = []
 # for i in range(len(random_transformed)):
