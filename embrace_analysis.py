b337a064c6c0713a99718824ad4e8f98a1cc815f
ian <ian@embracetech.co>
2017-10-29 16:08:34 -0700

    Start of embrace package

---
+++ embrace_analysis.py
@@ -0,0 +1,81 @@
+import numpy as np
+import matplotlib as plt
+import embrace_setup as es
+import embrace_fft as ef
+import embrace_metrics as em
+
+#default window size for kmeans clustering
+WIN_SIZE = 50
+#default number of clusters for walk cycles
+NUM_CLUSTERS = 8
+
+def kmeans_window(signal, win_size=WIN_SIZE):
+    """Get a list containing all the windows of size win_size from the signal
+       Input:
+         signal - 1-D array containing a time series signal
+         win_size - length of the sliding window
+       Output:
+         wins - list of windows """
+    wins = []
+    for i in range(len(signal)-win_size):
+        wins.append(signal[i:i+win_size])
+    wins = np.array(wins)
+    return wins
+
+def graph_kmeans(state_seq):
+    plt.plot(range(len(state_seq)), state_seq)
+    plt.show()
+
+def kmeans_walk(vals, num_clusters=NUM_CLUSTERS):
+    #list to hold the windows over all signals
+    wins = []
+
+    #transpose ac,gy,and mag
+    pos = 1
+    for v in vals[1:4]:
+        vals[pos] = np.transpose(v)
+        pos += 1
+
+    #iterate through the 3 subdimensions for ac,gy,mag
+    for v in vals[1:4]:
+        for r in v:
+            tmp = get_windows(r)
+            wins.append(get_windows(r))
+
+    #the magnitude vectors are 1 dimensional
+    for i,v in enumerate(vals[4:7]):
+        wins.append(get_windows(v))
+
+    #convert to a numpy array and reshape as (num_wins,num_signals,win_size)
+    wins = np.array(wins)
+    wins = np.swapaxes(wins,0,1)
+
+    #prepare the training matrix
+    X = []
+    for win in wins:
+        tmp = np.empty(0)
+        for sig in win:
+            tmp = np.concatenate((tmp,sig))
+        X.append(tmp)
+    X = np.array(X)
+
+    #create the kmeans clusterer and fit X to it
+    kmeans = KMeans(n_clusters=8)
+    kmeans.fit(X)
+
+    #get the predicted states for each window
+    state_seq = kmeans.predict(X)
+    
+    #plot the results
+    graph_kmeans(state_seq)
+
+    inds = np.ndarray.tolist(np.where(labs == 0)[0])
+    i = 0
+    while i < len(inds)-1:
+        if inds[i] == inds[i+1]-1:
+            del inds[i]
+        else:
+            i += 1
+    #a rough measurement of steps
+    print(len(inds))
+
---
+++ embrace_metrics.py
@@ -0,0 +1,126 @@
+#!/usr/bin/python
+
+import sys
+import numpy as np
+import scipy.stats as stats
+import matplotlib.pyplot as plt
+import embrace_setup as es
+
+#get the constants out the way
+THRESH_LOW = 1
+#THRESH_LOW = .01
+WIN_LEN = 50
+BACK_TRACK = 15
+STEP_SIZE = 13 #step size for step (walking) detection
+#STEP_SIZE = 50
+
+def merge_ics(ics):
+    """Merge hic or lics. Just call me if you want to talk about this algorithm + get_lics"""
+    i = 0
+    while i < len(ics)-1:
+        if ics[i][1] == ics[i+1][0]:
+            ics[i] = (ics[i][0],ics[i+1][1])
+            ics.pop(i+1)
+        else:
+            i += 1
+    return ics
+
+def get_lics(signal, step_size=STEP_SIZE, thresh=THRESH_LOW):
+    """Scans the signal in steps of STEP_SIZE. If the windows variance is less the THESH_LOW, the window is labeled as a lic. Otherwise, it is labeled as a hic. Consecutive lics and hics are combined to yield the final output
+    
+    Input: 
+        signal - column vector containing data points
+        step_size - the size of the windows. Defaults to 14
+        thresh - the thresh hold for a low information winow. Defaults to 1
+        
+    Output:
+        lics - list of tuples holding (start, stop) of lics
+        hics - list of tuples holding (start, stop) of hics"""
+    
+    lics = []
+    hics = []
+    i = 0
+    while i < len(signal) - step_size:
+        if np.var(signal[i:i+step_size]) <= thresh:
+            lics.append((i,i+step_size))
+        else:
+            hics.append((i,i+step_size))
+        i += step_size
+    #combine sequential hic and lic windows
+    hics = merge_ics(hics)
+    lics = merge_ics(lics)
+
+    return [lics,hics]
+
+def plot_var(signal,win_len=WIN_LEN):
+    """Plot the variance of a sliding window of size WIN_LEN
+        Input:
+            signal - the signal to plot
+            win_len - the size of the sliding window"""
+    variances = []
+    for i in range(len(signal)-win_len):
+        variances.append(np.var(signal[i:i+ein_len]))
+    plt.title("Variance of Window - Size " + str(win_len))
+    plt.plot(range(len(variances)), variances)
+    plt.show()
+
+def calc_step_var(vals):
+    """caculate the percentage of step time spent on each foot. Also
+    displays the variance between step locations for each foot.
+        Input:
+            vals - the ouput of embrace_setup.get_lists(file)"""
+    t,ac,gy,mag,ac_mag,gy_mag,mag_mag = vals
+
+    max_ac_amp = max(ac_mag)
+    max_ac_amps = []
+    max_gy_amp = max(gy_mag)
+
+
+
+    lics, hics = get_lics(ac_mag)
+    print(len(lics))
+    print(len(hics))
+
+    #average time between steps
+    f1 = []
+    f2 = []
+    flag = 0
+    for i in range(2,len(hics)-1):
+        if flag == 0:
+            f1 += [hics[i][0] - hics[i-1][0]]
+        else:
+            f2 += [hics[i][0] - hics[i-1][0]]
+        flag = 0 if flag == 1 else 1
+
+    f1_av = np.average(f1)
+    f2_av = np.average(f2)
+    f1_sd = np.var(f1)**.5
+    f2_sd = np.var(f2)**.5
+    
+    #discard outlier, where an outlier falls outside the window
+    # average +- sig_mult*sigma
+    sig_mult = 1.5
+    f1_l = f1_av-(sig_mult*f1_sd)
+    f1_h = f1_av+(sig_mult*f1_sd)
+    f2_l = f2_av-(sig_mult*f2_sd)
+    f2_h = f2_av+(sig_mult*f2_sd)
+
+    #filter the data
+    f1 = [ v for v in f1 if f1_l <= v and f1_h >= v]
+    f2 = [ v for v in f2 if f2_l <= v and f2_h >= v]
+
+    f_1 = sum(f1)/len(f1)
+    f_2 = sum(f2)/len(f2)
+    f3 = f_1 + f_2
+    f_1 /= f3 * .01
+    f_2 /= f3 * .01
+    #print out some metrics
+    print("Number of steps counted: " + str(len(hics)))
+    print("f1: " + str(f_1))
+    print("f1 sd: " + str(np.var(f1)**.5))
+    print("f2: " + str(f_2))
+    print("f2 sd: " + str(np.var(f2)**.5))
+
+
+
+
