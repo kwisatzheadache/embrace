d5c290c9d08e8d4a7d7d4a3cedf257075b3072a4
kwisatzheadache <michaelhorvath87@gmail.com>
2017-11-05 13:48:57 -0800

    kld testing

--- new_import.py
+++ new_import.py
@@ -1,10 +1,9 @@
 execfile('functions.py')
 
-stand_sit = get_nx10('data/stand_sit.csv')
 walk = get_nx10('data/walk.csv')
-sit = get_nx10('data/sits.csv')
-stand = get_nx10('data/stands.csv')
-random = get_nx10('data/random_seq.csv')
+
+walk = np.array(walk)
+walk = walk.transpose()
 
 # Set window and freqency values and labels
 window = 100
@@ -15,21 +14,11 @@ stand_label = 3
 bin_size = 100
 
 
-# Datasets to windows
-stand_sit = dataset_to_windows(stand_sit, window)
-fft = fft_transform(stand_sit)
+binned = make_bins(walk, bin_size)
 
-"""----------------- TRANSFORM DATA ------------------"""
-walk = data_transform(walk, window, freq)
-sit = data_transform(sit, window, freq)
-stand = data_transform(stand, window, freq)
+""" determine optimal reduction of signal"""
+run_klds(binned[1][1])
 
-stand_sit = data_transform(stand_sit, window, freq)
-random = data_transform(random, window, freq)
-
-"""----------------- LABEL Y-VALUES --------------------""" 
-# Has y-values
-walk = label_y(walk, walk_label)
-sit = label_y(sit, sit_label)
-stand = label_y(stand, stand_label)
+""" 33 dominant freqencies maintains kld entropy less than .05
+Check other signals to determine if this holds"""
 
