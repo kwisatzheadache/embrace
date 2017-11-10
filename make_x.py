d8f630e1e8778f1616bff259e2102eed0170c3d6
kwisatzheadache <michaelhorvath87@gmail.com>
2017-11-10 14:45:42 -0800

    magnitudes added

--- make_x.py
+++ make_x.py
@@ -1,29 +1,19 @@
 X_walk = []
 
-# walk_array = np.array(dom_walk[coords])
-# for i in range(len(walk_array)):
-#     freqs = []
-#     coeffs = []
-#     for j in range(len(walk_array[i])):
-#         Fk, n_freq = walk_array[i,j]
-#         for l in range(len(Fk)):
-#             coeffs.append(Fk[l])
-#             freqs.append(n_freq[l])
-#     X_walk.append(inp)
-#     X_walk.append(dom_walk['id'][i])
+df = DataFrame(dom_walk[coords])
 
-walk_array = np.array(dom_walk[coords])
-Fks = []
-n_freqs = []
-for i in range(len(walk_array)):
-    for j in range(len(walk_array[i])):
-        Fk, n_freq = walk_array[i,j]
+for i in df.columns:
+    Fks = []
+    n_freqs = []
+    for j in range(len(df[i])):
+        Fk, n_freq = df[i][j]
         Fks.append(Fk)
         n_freqs.append(n_freq)
+    df[i+'coeffs'] = Fks
+    df[i+'n_freqs'] = n_freqs
 
 X_walk = np.array(X_walk)
 
-acc_mag, mag_mag = get_mags(walk)
-# X_walk['acc_mag'] = acc_mag
-# X_walk['mag_mag'] = mag_mag
-# X_walk = np.hstack([X_walk, acc_mag, mag_mag])
+acc_mag, mag_mag = get_mags(dom_walk)
+df['acc_mag'] = acc_mag
+df['mag_mag'] = mag_mag
