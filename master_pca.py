a16df873d7c8fc936a30dbc9a0877af953390a49
kwisatzheadache <michaelhorvath87@gmail.com>
2017-11-14 15:48:47 -0800

    generate features

---
+++ master_pca.py
@@ -0,0 +1,36 @@
+import sys
+
+# csv_directory = sys.argv[1]
+window = 100
+num_freqs = 33
+
+# masterset = []
+# for filename in os.listdir(csv_directory):
+#     features = generate_features(filename, window, doms)
+#     masterset.append(features)
+
+# pca = run_pca(masterset)
+
+def generate_features(filename):
+    data = get_nx10(filename)
+    doms = data_transform(data, window, num_freqs)
+    coords = doms.columns[:9]
+    for i in coords:
+        Fks = []
+        n_freqs = []
+        for j in range(len(doms[i])):
+            Fk, n_freq = doms[i][j]
+            Fk = abs(Fk)
+            Fks.append(Fk)
+            n_freqs.append(n_freq)
+
+        doms[i+'coeffs'] = Fks
+        doms[i+'n_freqs'] = n_freqs
+    doms = doms.drop(coords, axis=1)
+    columns = data.columns
+    X_data = np.array(data)
+    for i in doms.columns:
+        arrays = np.array([np.array(x) for x in doms[i]])
+        X_walk = np.hstack([X_data, arrays])
+    return X_data
+
