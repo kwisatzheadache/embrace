b60b7cc9525ed6232df633ebfbd3c5d96b723c39
kwisatzheadache <michaelhorvath87@gmail.com>
2017-12-05 19:38:18 -0800

    works

--- straight.py
+++ straight.py
@@ -16,8 +16,24 @@ def stack_walks(direc):
     complete = np.vstack([get_nx10(direc+'/'+x) for x in csvs])
     return complete
 
-complete = stack_walks('./demo_data/walk/ryan')
+def return_straight_walk(walk_file, angle):
+    """
+    Return windows where all values fall within the range of +/- rotation angle around the average.
+    """
+    rotation = (.72*angle)/360
+    csv = get_nx10(walk_file)
+    mag_z = csv[:,9]
+    lagged = lag(mag_z, 100)
+    straight = []
+    for i in lagged:
+        avg = np.mean(i)
+        low = avg - rotation
+        high = avg + rotation
+        if all(low <= j <= high for j in i):
+            straight.append(i)
+    return np.array(straight)
 
+complete = stack_walks('./demo_data/walk/ryan')
 # Grab the individual magnetometer readings
 mag_x = complete[:,7]
 mag_y = complete[:,8]
@@ -33,8 +49,10 @@ for x in [mag_x, mag_y, mag_z]:
 
 lagged = lag(mag_z, 100)
 
+# Set the threshold determining straight. The range of the magnetometer is .73, so a 30 degree window corresponds to .03 above and below the average.
 rotation = .03
 
+# If all the values in window fall within the range from low to high, add window to straight set.
 straight = []
 for i in lagged:
     avg = np.mean(i)
