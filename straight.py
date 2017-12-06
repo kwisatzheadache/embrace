4ddfa379e44260ee5c55a1177eb7e998acf6d933
kwisatzheadache <michaelhorvath87@gmail.com>
2017-12-05 19:45:58 -0800

    modularized straightwalk

--- straight.py
+++ straight.py
@@ -16,13 +16,18 @@ def stack_walks(direc):
     complete = np.vstack([get_nx10(direc+'/'+x) for x in csvs])
     return complete
 
-def return_straight_walk(walk_file, angle):
+def return_straight_walk(walk_input, angle):
     """
+    Input: walk_directory, angle
+    Alternately, instead of directory, use nx10 data in np.array
     Return windows where all values fall within the range of +/- rotation angle around the average.
     """
     rotation = (.72*angle)/360
-    csv = get_nx10(walk_file)
-    mag_z = csv[:,9]
+    if isinstance(walk_input, str):
+        walks = stack_walks(walk_input)
+    else:
+        walks = walk_input
+    mag_z = walks[:,9]
     lagged = lag(mag_z, 100)
     straight = []
     for i in lagged:
@@ -32,33 +37,3 @@ def return_straight_walk(walk_file, angle):
         if all(low <= j <= high for j in i):
             straight.append(i)
     return np.array(straight)
-
-complete = stack_walks('./demo_data/walk/ryan')
-# Grab the individual magnetometer readings
-mag_x = complete[:,7]
-mag_y = complete[:,8]
-mag_z = complete[:,9]
-
-# Print descriptive statistics
-for x in [mag_x, mag_y, mag_z]:
-    print(stats.describe(x))
-
-# Need to separate walks into chunks, where all mag_z values are within .1 of the average.
-# Perhaps use std deviation of chunks... Where std < .05?
-# Make windows... std of windows?
-
-lagged = lag(mag_z, 100)
-
-# Set the threshold determining straight. The range of the magnetometer is .73, so a 30 degree window corresponds to .03 above and below the average.
-rotation = .03
-
-# If all the values in window fall within the range from low to high, add window to straight set.
-straight = []
-for i in lagged:
-    avg = np.mean(i)
-    low = avg - rotation
-    high = avg + rotation
-    if all(low <= j <= high for j in i):
-        straight.append(i)
-
-print(np.array(straight).shape)
