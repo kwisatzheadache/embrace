415bb817efce1b056cea5b205649f57eaf2c10f5
kwisatzheadache <michaelhorvath87@gmail.com>
2017-12-16 16:00:09 -0800

    line 61 - why isn't it returning the longest? Also, change values to indices, rather than averages.

--- straight.py
+++ straight.py
@@ -32,10 +32,41 @@ def straight_walk(walk_input, angle):
     mag_z = walks[:,9]
     lagged = lag(mag_z, 100)
     straight = []
+    index = []
     for i in range(len(lagged)):
         avg = np.mean(lagged[i])
         low = avg - rotation
         high = avg + rotation
         if all(low <= j <= high for j in lagged[i]):
             straight.append(array[i])
-    return np.array(straight)
+            index.append(avg)
+    return np.array(straight), index
+
+def index_to_keep(index, acc=[]):
+    if abs(index[0] - index[1]) < .01:
+        acc.append(index[0])
+    # lagged = lag(index, 2)
+    # long1 = []
+    # for i in range(len(lagged)):
+    #     j, k = i
+    #     if abs(j - k) < .01:
+    #         long1.apend(i)
+    #     else:
+
+
+def loop_over_index(index, acc1, longest):
+    if len(index) < 2:
+        print('here')
+        output = longest
+        return(output)
+    else:
+        i = index[0]
+        j = index[1]
+        if abs(i - j) < .01:
+            acc1.append(index[0])
+            loop_over_index(index[1:], acc1, longest)
+        else:
+            if len(acc1) >= len(longest):
+                loop_over_index(index[1:], [], acc1)
+            else:
+                loop_over_index(index[1:], [], longest)
