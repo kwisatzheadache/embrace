9342f078cce8e144eb734df43592400667324586
kwisatzheadache <michaelhorvath87@gmail.com>
2017-12-28 10:49:12 -0800

    cleanup

--- autocorr.py
+++ autocorr.py
@@ -2,32 +2,8 @@ from scipy.signal import find_peaks_cwt
 execfile('functions.py')
 execfile('peakdet.py')
 
-# dire = './demo_data/walk'
+dire = './demo_data/walk'
 straight, index = straight_walk(dire, 15)
 longest = longest_walk(straight, index)
 
-# df = DataFrame(longest)
-# cols = ['id', 'acc_x', 'acc_y', 'acc_z', 'gy_x', 'gy_y', 'gy_z', 'mag_x', 'mag_y', 'mag_z'] 
-# df.columns = cols
-
-# acc_mag, mag_mag, mag_gy = get_mags(df)
-# reduced_acc = reduce_signal(acc_mag)
-# reduced_mag = reduce_signal(mag_mag)
-# reduced_gy = reduce_signal(mag_gy)
-
-# acc = [make_auto(acc_mag), make_auto(reduced_acc)]
-# mag = [make_auto(mag_mag), make_auto(reduced_mag)]
-# gy = [make_auto(mag_gy), make_auto(reduced_gy)]
-
-# plt.plot(gy[0])
-# plt.plot(gy[1])
-# plt.show()
-
-
-# maxtab, mintab = peakdet(gy[1])
-
-# ind = maxtab[:, 0]
-# ind = ind.astype(int)
-
-# len_btw_steps = [j-i for i, j in zip(ind[:-1], ind[1:])]
-
+steps, len_steps = find_steps(longest)
