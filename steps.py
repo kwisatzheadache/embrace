a97cd17d9a26a80607b0e3fbd178cb8cf76e6293
kwisatzheadache <michaelhorvath87@gmail.com>
2018-01-01 13:03:55 -0800

    finds step length, number of steps, left/right ratio

--- steps.py
+++ steps.py
@@ -12,11 +12,13 @@ straights, index = straight_walk(dire, 15)
 longest = longest_walk(straights, index)
 
 steps = find_steps(longest)
-lens = lens(steps)
+# lens = lens(steps)
 
 a, b = left_right(steps)
 
+avg_step = avg_len(steps)
 avg_a = avg_len(a)
 avg_b = avg_len(b)
 
 ratio_ab = avg_a/float(avg_b)
+
